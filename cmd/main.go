5e9d657d6d603274f4f92476f900e3657c1b6969
Denis Muhortov <muhortovdenis48@gmail.com>
2021-10-29 12:54:19 +0300

    Иниц

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main(){
-	
+import (
+	"WBproj/pkg"
+	"log"
+)
+
+func main() {
+	srv := new(pkg.Server)
+	if err := srv.Run("8080"); err != nil {
+		log.Fatal(err.Error())
+	}
 }
