e54d5ca54f2672b45e35c5249cb542295967dcf8
Denis Muhortov <muhortovdenis48@gmail.com>
2021-11-14 15:25:30 +0300

    Получаю из потока по под подписке данные

--- go.mod
+++ go.mod
@@ -2,14 +2,16 @@ module WBproj
 
 go 1.17
 
-require github.com/nats-io/stan.go v0.10.1
+require (
+	github.com/nats-io/nats.go v1.13.1-0.20211018182449-f2416a8b1483
+	github.com/nats-io/stan.go v0.10.1
+)
 
 require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/nats-io/nats-server/v2 v2.6.4 // indirect
 	github.com/nats-io/nats-streaming-server v0.23.1 // indirect
-	github.com/nats-io/nats.go v1.13.1-0.20211018182449-f2416a8b1483 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
--- main.go
+++ main.go
@@ -3,19 +3,46 @@ package main
 import (
 	"log"
 
+	"github.com/nats-io/nats.go"
 	"github.com/nats-io/stan.go"
 )
 
+
+func printMsg(m *stan.Msg, i int) {
+	log.Printf("[#%d] Received: %s\n", i, m)
+}
 func main() {
+	var i int = 0
 	ClusterURLs = [2]string{
 		"wbx-world-nats-stage.dp.wb.ru",
 		"wbx-world-nats-stage.dl.wb.ru",
 	}
 	ClusterID = "world-nats-stage"
 	Subject = "go.test"
-	conn, err := stan.Connect(ClusterURLs[0], ClusterID)
+	opts := []nats.Option{nats.Name("NATS Streaming Example Subscriber")}
+	nc, err := nats.Connect(ClusterURLs[0], opts...)
 	if err != nil {
-		log.Fatal("err")
+		log.Fatal(err)
 	}
-	defer conn.Close()
+	defer nc.Close()
+
+	sc, err := stan.Connect(ClusterID, "stan-sub", stan.NatsConn(nc),
+		stan.SetConnectionLostHandler(func(_ stan.Conn, reason error) {
+			log.Fatalf("Connection lost, reason: %v", reason)
+		}))
+	if err != nil {
+		log.Fatalf("Can't connect: %v.\nMake sure a NATS Streaming Server is running at: %s", err, ClusterURLs[0])
+	}
+	log.Printf("Connected to %s clusterID: [%s] clientID: [%s]\n", ClusterURLs[0], ClusterID, Subject)
+
+	sub, err := sc.Subscribe(Subject, func(m *stan.Msg) {
+		printMsg(m, i)
+		i++
+	}, stan.DeliverAllAvailable())
+	if err != nil {
+		sc.Close()
+		log.Fatal(err)
+	}
+	defer sc.Close()
+	defer sub.Close()
 }
